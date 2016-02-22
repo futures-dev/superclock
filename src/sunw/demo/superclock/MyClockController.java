48ad73aedf20e0c4bb86f7102f204ff3f0cacb58
Andrei@Computer <futures_dev@outlook.com>
2016-02-22 23:00:54 +0300

    todo: timer update

--- src/sunw/demo/superclock/MyClockController.java
+++ src/sunw/demo/superclock/MyClockController.java
@@ -1,7 +1,43 @@
 package sunw.demo.superclock;
 
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Andrei on 22.02.2016.
  */
 public class MyClockController {
+
+    private final int PERIOD = 2000;
+    MyClockView view;
+    Timer secondTimer = new Timer("second");
+
+    Calendar calendar = new GregorianCalendar();
+
+    public MyClockController() {
+        secondTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (view != null) {
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    view.setLine3(calendar.get(Calendar.SECOND));
+                    view.setLine2(calendar.get(Calendar.MINUTE));
+                    view.setLine1(3 * calendar.get(Calendar.HOUR) + calendar.get(Calendar.MINUTE) / 20);
+                    view.setDayNumber(String.format("%02d", calendar.get(Calendar.DAY_OF_MONTH)));
+                    view.setAM_PM(calendar.get(Calendar.AM_PM));
+                }
+            }
+        }, 0, PERIOD);
+    }
+
+    public void setView(MyClockView view) {
+        this.view = view;
+    }
+
+    public void setTimeZone(String timeZone) {
+
+    }
 }
--- src/sunw/demo/superclock/MyClockView.java
+++ src/sunw/demo/superclock/MyClockView.java
@@ -14,10 +14,71 @@ import java.io.IOException;
  */
 
 public class MyClockView extends Pane {
+    //region width
+    public static final Double DEFAULT_WIDTH = 500.0;
+    //region height
+    public static final Double DEFAULT_HEIGHT = 500.0;
+    //region radius
+    public static final Double DEFAULT_RADIUS = 200.0;
+    //region backColorPM
+    public static final String DEFAULT_BACKCOLORPM = "#FFFF00";
+    //region backColorAM
+    public static final String DEFAULT_BACKCOLORAM = "#0000FF";
+    //region currentBackColor
+    public static final String DEFAULT_CURRENTBACKCOLOR = DEFAULT_BACKCOLORAM;
+    //endregion
+    public static final String DEFAULT_AM_PM = "AM";
+    public static final String DEFAULT_city = "Moscow (UTC+03:00)";
+    public static final String DEFAULT_dayNumber = "01";
+    //region x1
+    public static final Double DEFAULT_x1 = .0;
+    //endregion
+    //region y1
+    public static final Double DEFAULT_y1 = -200.0;
+    //region x2
+    public static final Double DEFAULT_x2 = 150.0;
+    //region y2
+    public static final Double DEFAULT_y2 = 0.0;
+    //region x3
+    public static final Double DEFAULT_x3 = -100.0;
+    //endregion
+
+    //region backColor
+    //region y3
+    public static final Double DEFAULT_y3 = .0;
+    MyClockController controller = new MyClockController();
+    private DoubleProperty _width = new SimpleDoubleProperty(DEFAULT_WIDTH);
+    private DoubleProperty _height = new SimpleDoubleProperty(DEFAULT_HEIGHT);
+    //endregion
+    private DoubleProperty _radius = new SimpleDoubleProperty(DEFAULT_RADIUS);
+    private StringProperty _backColorPM = new SimpleStringProperty(DEFAULT_BACKCOLORPM);
+    private StringProperty _backColorAM = new SimpleStringProperty(DEFAULT_BACKCOLORAM);
+    private StringProperty _currentBackColor = new SimpleStringProperty(DEFAULT_CURRENTBACKCOLOR);
+    //endregion
+    private StringProperty _AM_PM = new SimpleStringProperty(DEFAULT_AM_PM);
+    private StringProperty _city = new SimpleStringProperty(DEFAULT_city);
+    private StringProperty _dayNumber = new SimpleStringProperty(DEFAULT_dayNumber);
+    private DoubleProperty _x1 = new SimpleDoubleProperty(DEFAULT_x1);
+    //endregion
+
+    //endregion
+
+    //region AM_PM
+    private DoubleProperty _y1 = new SimpleDoubleProperty(DEFAULT_y1);
+    private DoubleProperty _x2 = new SimpleDoubleProperty(DEFAULT_x2);
+    private DoubleProperty _y2 = new SimpleDoubleProperty(DEFAULT_y2);
+    private DoubleProperty _x3 = new SimpleDoubleProperty(DEFAULT_x3);
+    private DoubleProperty _y3 = new SimpleDoubleProperty(DEFAULT_y3);
+
+    //endregion
+
+
+    //region city
+
     public MyClockView() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                 "MyClock.fxml"));
-        fxmlLoader.getNamespace().put("bind",this);
+        fxmlLoader.getNamespace().put("bind", this);
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
         try {
@@ -25,29 +86,175 @@ public class MyClockView extends Pane {
         } catch (IOException exception) {
             //throw new RuntimeException(exception);
         }
+        controller.setView(this);
         InitializeComponents();
     }
-
-    public static final double DEFAULT_WIDTH = 200;
-    private DoubleProperty _width = new SimpleDoubleProperty(DEFAULT_WIDTH);
 
     public final double getWidth_() {
         return _width.get();
     }
 
-    public static final double DEFAULT_HEIGHT = 200;
-    private DoubleProperty _height = new SimpleDoubleProperty(DEFAULT_HEIGHT);
+    public final void setWidth_(Double value) {
+        _width.set(value);
+    }
 
     public final double getHeight_() {
         return _height.get();
     }
 
-    public static final String DEFAULT_BACKCOLOR = "#FF0000";
-    private StringProperty _backcolor = new SimpleStringProperty(DEFAULT_BACKCOLOR);
+    //endregion
 
-    public final String getBackColor() {
-        return _backcolor.get();
+    //region dayNumber
+
+    public final void setHeight_(Double value) {
+        _height.set(value);
     }
+
+    public final double getRadius_() {
+        return _radius.get();
+    }
+
+    public final void setRadius_(Double value) {
+        _radius.set(value);
+    }
+
+    public final String getBackColorPM() {
+        return _backColorPM.get();
+    }
+
+    //endregion
+
+    //region Arrows
+
+    public final void setBackColorPM(String value) {
+        _backColorPM.set(value);
+    }
+
+    public final String getBackColorAM() {
+        return _backColorAM.get();
+    }
+
+    public final void setBackColor(String value) {
+        _backColorAM.set(value);
+    }
+
+    public final String getCurrentBackColor() {
+        return _currentBackColor.get();
+    }
+    //endregion
+
+    public final void setCurrentBackColor(String value) {
+        _currentBackColor.set(value);
+    }
+
+    public final String getAM_PM() {
+        return _AM_PM.get();
+    }
+
+    public void setAM_PM(int a) {
+        setAM_PM(a == 0 ? "AM" : "PM");
+    }
+
+    public final void setAM_PM(String value) {
+        _AM_PM.set(value);
+        if (value.equals("AM")) {
+            _currentBackColor.set(_backColorAM.get());
+        } else {
+            _currentBackColor.set(_backColorPM.get());
+        }
+    }
+    //endregion
+
+    public final String getCity() {
+        return _city.get();
+    }
+
+    public final void setCity(String value) {
+        _city.set(value);
+    }
+
+    public final String getDayNumber() {
+        return _dayNumber.get();
+    }
+
+    public final void setDayNumber(String value) {
+        _dayNumber.set(value);
+    }
+
+    public final Double getX1() {
+        return _x1.get();
+    }
+    //endregion
+
+    public final void setX1(Double value) {
+        _x1.set(value);
+    }
+
+    public final Double getY1() {
+        return _y1.get();
+    }
+
+    public final void setY1(Double value) {
+        _y1.set(value);
+    }
+
+    public void setLine1(int hour) {
+        // hour is [0;36)
+        double angle = Math.toRadians(hour * 10);
+        setX1(getRadius_() * Math.sin(angle));
+        setY1(-getRadius_() * Math.cos(angle));
+    }
+    //endregion
+
+    public final Double getX2() {
+        return _x2.get();
+    }
+
+    public final void setX2(Double value) {
+        _x2.set(value);
+    }
+
+    public final Double getY2() {
+        return _y2.get();
+    }
+
+    public final void setY2(Double value) {
+        _y2.set(value);
+    }
+
+    public void setLine2(int minute) {
+        // minute is [0;60)
+        double angle = Math.toRadians(minute * 6);
+        setX1(getRadius_() * Math.sin(angle));
+        setY1(-getRadius_() * Math.cos(angle));
+    }
+    //endregion
+
+    public final Double getX3() {
+        return _x3.get();
+    }
+
+    public final void setX3(Double value) {
+        _x3.set(value);
+    }
+
+    public final Double getY3() {
+        return _y3.get();
+    }
+
+    public final void setY3(Double value) {
+        _y3.set(value);
+    }
+    //endregion
+
+    public void setLine3(int second) {
+        // second is [0;60)
+        double angle = Math.toRadians(second * 6);
+        setX1(getRadius_() * Math.sin(angle));
+        setY1(-getRadius_() * Math.cos(angle));
+    }
+
+    //endregion
 
 
     private void InitializeComponents() {
